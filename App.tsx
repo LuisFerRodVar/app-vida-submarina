2bb17b043b8703de1858dd68169d584c66b2b9e0
LuisFerRodVar <luisfrv1@estudiantec.cr>
2024-06-20 10:45:24 -0600

    Reorganization of file tree and added navigation funcionality

--- App.tsx
+++ App.tsx
@@ -1,22 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/screens/Login';
+import Register from './src/screens/Register';
+import Home from './src/screens/Home';
+import Recovery from './src/screens/Recovery';
+import RecoveryAux from './src/screens/RecoveryAux';
+import Configurations from './src/screens/Configurations';
+import Donations from './src/screens/Donations';
+import News from './src/screens/News';
+import Profile from './src/screens/Profile';
+import Species from './src/screens/Species';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name="Home" component={Home} options={{headerShown:false}}/>
+        <Stack.Screen name="Login" component={Login} options={{headerShown:false}}/>
+        <Stack.Screen name="Register" component={Register} options={{headerShown:false}}/>
+        <Stack.Screen name="Recovery" component={Recovery} options={{headerShown:false}}/>
+        <Stack.Screen name="RecoveryAux" component={RecoveryAux} options={{headerShown:false}}/>
+        <Stack.Screen name="Configurations" component={Configurations} options={{headerShown:false}}/>
+        <Stack.Screen name="Donations" component={Donations} options={{headerShown:false}}/>
+        <Stack.Screen name="News" component={News} options={{headerShown:false}}/>
+        <Stack.Screen name="Profile" component={Profile} options={{headerShown:false}}/>
+        <Stack.Screen name="Species" component={Species} options={{headerShown:false}}/>
+      </Stack.Navigator>
+   </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/screens/Donations.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Donations() {
+  return (
+    <View>
+      <Text>Donations</Text>
+    </View>
+  )
+}
