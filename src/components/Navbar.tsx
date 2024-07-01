4f95567a6a9b23a3acd09f2791e061a0e270c8ca
LuisFerRodVar <luisfrv1@estudiantec.cr>
2024-07-01 08:20:40 -0600

    Added navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,49 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
-import { FontAwesome } from '@expo/vector-icons'
 import { MaterialIcons } from '@expo/vector-icons'
+import { StyleSheet } from 'react-native'
 
 export default function Navbar() {
   return (
-    <View>
-    <TouchableOpacity>
-      <MaterialIcons name='home' size={24} color='black' />
-    </TouchableOpacity>
-    <TouchableOpacity>
-      <MaterialIcons name='newspaper' size={24} color='black' />
-    </TouchableOpacity>
-    <TouchableOpacity>
-      <MaterialIcons name='waves' size={24} color='black' />
-    </TouchableOpacity>
-    <TouchableOpacity>
-      <MaterialIcons name='savings' size={24} color='black' />
-    </TouchableOpacity>
-    <TouchableOpacity>
-      <MaterialIcons name='menu' size={24} color='black' />
-    </TouchableOpacity>
+    <View style={styles.container}>
+      <View style={styles.navbar}>
+        <TouchableOpacity>
+          <MaterialIcons name='home' size={24} color='black' />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <MaterialIcons name='newspaper' size={24} color='black' />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <MaterialIcons name='waves' size={24} color='black' />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <MaterialIcons name='savings' size={24} color='black' />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <MaterialIcons name='menu' size={24} color='black' />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  navbar: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+    backgroundColor: 'white',
+    borderTopWidth: 1,
+    borderTopColor: 'lightgray',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  container: {
+    height: '95%',
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+
+})
