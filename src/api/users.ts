e031b43dab35f7b3e8e9be76c5baadfc5ccf20a3
LuisFerRodVar <luisfrv1@estudiantec.cr>
2024-07-01 22:44:01 -0600

    Added fetch call for login

---
+++ src/api/users.ts
@@ -0,0 +1,25 @@
+const URL = 'http://10.0.2.2:8080/api/'; 
+export async function login(username: string, password: string) {
+  let data = {email: username, password: password, username: "", email_2:""};
+  
+  try {
+    const response = await fetch(URL + 'login', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(`Network response was not ok: ${response.status} ${response.statusText}. Server response: ${errorText}`);
+    }
+
+    return await response.text();
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+    throw error;
+  }
+}
+
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,12 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import Navbar from '../components/Navbar'
-
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet } from 'react-native'
+import { login } from '../api/users'
 export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleLogin = () => {
+    login(username, password).then(data => {
+      alert(data);
+    });
+  }
+
   return (
     <View>
-      <Text>Login</Text>
-      <Navbar />
+      <Text style={Styles.title}>Vida Submarina</Text>
+      <Text style={Styles.subtitle}>Iniciar sesión</Text>
+      <TextInput placeholder='Correo electrónico' onChangeText={setUsername} />
+      <TextInput placeholder='Contraseña' onChangeText={setPassword} />
+      <TouchableOpacity onPress={handleLogin}>
+        <Text>Iniciar sesión</Text>
+      </TouchableOpacity>
     </View>
   )
 }
+const Styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 20
+  },
+  subtitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 20
+  }
+  
+})
